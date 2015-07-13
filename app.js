9c099b0aaefe66803c29172c5564022d4c1b00ad
OstlerDev <ostlerdev@me.com>
2015-07-12 21:25:24 -0600

    Implement Updating

--- app.js
+++ app.js
@@ -18,12 +18,35 @@ function install(){
 	var git = require('gift');
 
 	git.clone('https://github.com/OstlerDev/PopcornTV.git', 'app/', function(err, repo){ 
-		if (err) document.write(err);
+		if (err) alert(err);
 
 		alert("Installed!");
-		document.getElementById('install').textContent = 'Update';
+		var button = document.getElementById('install');
+		button.textContent = 'Update';
+		button.onclick = app.update();
+
 	});
 }
+
+function update(){
+	var git = require('gift');
+	var repo = git('app');
+
+	var button = document.getElementById('install');
+	button.textContent = 'Updating...';
+
+	repo.sync(function(err){
+		if (err){
+		 	alert(err);
+		 	button.textContent = 'Update';
+		} else {
+			alert('Updated!');
+			button.textContent = 'Update';
+		}
+	});
+}
+
 exports.installed = installed;
 exports.start = start;
 exports.install = install;
+exports.update = update;
